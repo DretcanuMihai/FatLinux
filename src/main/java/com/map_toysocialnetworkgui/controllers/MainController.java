37e98f2a76f1e9b5bb06b0c9c3cc0938f39962eb
Mihai Dretcanu <mihai.dretcanu@stud.ubbcluj.ro>
2021-12-12 17:52:51 +0200

    Finished main window look

--- src/main/java/com/map_toysocialnetworkgui/controllers/MainController.java
+++ src/main/java/com/map_toysocialnetworkgui/controllers/MainController.java
@@ -1,4 +1,15 @@
 package com.map_toysocialnetworkgui.controllers;
 
 public class MainController extends AbstractController{
+    public void add() {
+    }
+
+    public void delete() {
+    }
+
+    public void accept() {
+    }
+
+    public void decline() {
+    }
 }
