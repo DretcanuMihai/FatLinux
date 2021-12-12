9e059839058c3538986cda61ea98edbc95cbba28
Livius Habuc <livius.habuc@gmail.com>
2021-12-12 19:25:06 +0200

    Added delete friend functionality

--- src/main/java/com/map_toysocialnetworkgui/controllers/MainController.java
+++ src/main/java/com/map_toysocialnetworkgui/controllers/MainController.java
@@ -6,24 +6,21 @@ import com.map_toysocialnetworkgui.model.entities_dto.UserDTO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
-
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
-public class MainController extends AbstractController{
-
-    //Data
+public class MainController extends AbstractController {
+    // Data
     UserDTO loggedUser;
     ObservableList<UserDTO> modelUsers = FXCollections.observableArrayList();
     ObservableList<UserDTO> modelFriends = FXCollections.observableArrayList();
     ObservableList<UserDTO> modelPending = FXCollections.observableArrayList();
-
-
-    //FXML
+    // FXML
     @FXML
     Label welcomeLabel;
     @FXML
@@ -45,9 +42,8 @@ public class MainController extends AbstractController{
     @FXML
     public TableColumn<UserDTO,String> usersLastNameColumn;
 
-
     @FXML
-    public void initialize(){
+    public void initialize() {
         usersFirstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
         usersLastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
         friendsFirstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
@@ -60,7 +56,7 @@ public class MainController extends AbstractController{
         pendingTable.setItems(modelPending);
     }
 
-    public void init(String userEmail){
+    public void init(String userEmail) {
         loggedUser=service.getUserDTO(userEmail);
         welcomeLabel.setText("Welcome, "+loggedUser.getFirstName()+"!");
         updateModelUsers();
@@ -68,27 +64,44 @@ public class MainController extends AbstractController{
         updateModelPending();
     }
 
-    private void updateModelUsers(){
+    private void updateModelUsers() {
         modelUsers.setAll(new ArrayList<>(service.getAllUserDTOs()));
     }
-    private void updateModelFriends(){
+
+    private void updateModelFriends() {
         modelFriends.setAll(service.getAllFriendshipDTOsOfUser(loggedUser.getEmail()).stream()
                 .map(FriendshipDTO::getUser2).collect(Collectors.toList()));
     }
-    private void updateModelPending(){
+
+    private void updateModelPending() {
         modelPending.setAll(service.getFriendRequestsSentToUser(loggedUser.getEmail()).stream()
                 .map(FriendRequestDTO::getSender).collect(Collectors.toList()));
     }
 
     public void add() {
+
     }
 
     public void delete() {
+        if (friendsTable.getSelectionModel().getSelectedItem() == null) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error!");
+            alert.setHeaderText("Empty selection error");
+            alert.setContentText("You must select a friend from friends table in order to remove it!");
+
+            alert.showAndWait();
+        } else {
+            String toDelete = friendsTable.getSelectionModel().getSelectedItem().getEmail();
+            service.deleteFriendship(loggedUser.getEmail(), toDelete);
+            updateModelFriends();
+        }
     }
 
     public void accept() {
+
     }
 
     public void decline() {
+
     }
 }
