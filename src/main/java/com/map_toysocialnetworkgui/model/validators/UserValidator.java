2bdf19bc75321dc4e259d0a0863b786b0c624236
Mihai Dretcanu <mihai.dretcanu@stud.ubbcluj.ro>
2021-12-19 16:54:30 +0200

    finished modifying user validator

--- src/main/java/com/map_toysocialnetworkgui/model/validators/UserValidator.java
+++ src/main/java/com/map_toysocialnetworkgui/model/validators/UserValidator.java
@@ -7,44 +7,51 @@ import java.util.regex.Pattern;
 /**
  * user entity validator
  */
-public class UserValidator implements Validator<User>{
-    static private final Pattern emailPattern =Pattern.compile("^[^@ ]+@[^@ ]+$");
-    static private final int maxSize=50;
+public class UserValidator implements Validator<User> {
+    static private final Pattern emailPattern = Pattern.compile("^[^@ ]+@[^@ ]+$");
+    static private final int maxSize = 50;
 
     /**
      * validates the user entity if:
-     * ->its email is non-null, non-empty, doesn't have commas and matches the email pattern
-     * ->its first name and last name are non-null, non-empty and don't have commas
+     * ->its email is non-null, non-empty but under 50 characters, and is a valid email
+     * ->its first name and last name are non-null, non-empty and under 50 characters
      * ->its passwordHash is not 0
+     *
      * @param entity - said entity
      * @throws ValidationException - if said entity is not valid
      */
     @Override
     public void validateD(User entity) throws ValidationException {
-        String message="";
-        String email=entity.getEmail();
-        String firstName=entity.getFirstName();
+        String message = "";
+        String email = entity.getEmail();
+        String firstName = entity.getFirstName();
         String lastName = entity.getLastName();
-        int passwordHash=entity.getPasswordHash();
-        if(email==null || email.equals("")|| !emailPattern.matcher(email).matches()
-                || email.length()>maxSize)
-            message+="Invalid email! Must be non empty, under 50 characters and" +
+        int passwordHash = entity.getPasswordHash();
+        if (email == null || email.equals("") || !emailPattern.matcher(email).matches()
+                || email.length() > maxSize)
+            message += "Invalid email! Must be non empty, under 50 characters and" +
                     " be a valid email address;\n";
-        if(firstName==null ||firstName.equals("") || firstName.length()>maxSize)
+        if (firstName == null || firstName.equals("") || firstName.length() > maxSize)
             message += "Invalid first name! Must be non empty and under 50 characters;\n";
-        if(lastName==null || lastName.equals("") || lastName.length() > maxSize)
+        if (lastName == null || lastName.equals("") || lastName.length() > maxSize)
             message += "Invalid last name! Must be non empty and under 50 characters;\n";
-        if(passwordHash==0)
-            message+="Invalid password! Must be non empty;\n";
-        if(!message.equals("")) {
-            message="Error:\n"+message;
+        if (passwordHash == 0)
+            message += "Invalid password! Must be non empty;\n";
+        if (!message.equals("")) {
+            message = "Error:\n" + message;
             throw new ValidationException(message);
         }
     }
 
-    public void validateEmail(String email)throws ValidationException {
-        if(email==null || email.equals("")|| !emailPattern.matcher(email).matches()
-                || email.length()>maxSize)
+    /**
+     * validates an email if said email is non-null, non-empty but under 50 characters, and is a valid email
+     *
+     * @param email - said email
+     * @throws ValidationException - if said email is not valid
+     */
+    public void validateEmail(String email) throws ValidationException {
+        if (email == null || email.equals("") || !emailPattern.matcher(email).matches()
+                || email.length() > maxSize)
             throw new ValidationException("""
                     Error:
                     Invalid email! Must be non empty, under 50 characters and be a valid email address;
