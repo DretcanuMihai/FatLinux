dbd73269aa0edf0cd457e40c3feb3ce982cccec2
Mihai Dretcanu <mihai.dretcanu@stud.ubbcluj.ro>
2021-12-19 15:16:49 +0200

    created an AccountStatus enum

---
+++ src/main/java/com/map_toysocialnetworkgui/model/entities/AccountStatus.java
@@ -0,0 +1,35 @@
+package com.map_toysocialnetworkgui.model.entities;
+
+/**
+ * enum describing the status of a user's account
+ */
+public enum AccountStatus{
+    ACTIVE(0),
+    DISABLED(1);
+    /**
+     * a unique code that describes the status
+     */
+    final int statusCode;
+
+    /**
+     * constructs an AccountStatus with a given status code
+     * @param statusCode - said status code
+     */
+    AccountStatus(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    /**
+     * returns the account status represented by a code
+     * @param code - said code
+     * @return the account status
+     */
+    public static AccountStatus getByCode(int code){
+        for(AccountStatus status:values()){
+            if(status.statusCode==code){
+                return status;
+            }
+        }
+        return null;
+    }
+}
