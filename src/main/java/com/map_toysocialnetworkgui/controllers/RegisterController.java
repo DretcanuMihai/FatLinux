99f99d85700684f6908ec43c2ce09392578767ec
Livius Habuc <livius.habuc@gmail.com>
2021-12-23 13:35:19 +0200

    Finished registration feature

--- src/main/java/com/map_toysocialnetworkgui/controllers/RegisterController.java
+++ src/main/java/com/map_toysocialnetworkgui/controllers/RegisterController.java
@@ -1,6 +1,10 @@
 package com.map_toysocialnetworkgui.controllers;
 
+import com.map_toysocialnetworkgui.model.entities_dto.UserServiceDTO;
+import com.map_toysocialnetworkgui.model.validators.ValidationException;
+import com.map_toysocialnetworkgui.service.AdministrationException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -23,8 +27,35 @@ public class RegisterController extends AbstractController {
     @FXML
     Label registerPasswordMatchErrorLabel;
 
-    public void signUp() throws IOException {
+    public void signUp() {
+        try {
+            String firstName = firstNameTextField.getText();
+            String lastName = lastNameTextField.getText();
+            String email = emailTextField.getText();
+            int passwordHash = passwordTextField.getText().hashCode();
+            int confirmPasswordHash = confirmPasswordTextField.getText().hashCode();
+            registerSuccessMessageLabel.setText("");
+            registerPasswordMatchErrorLabel.setText("");
 
+            if (passwordHash != confirmPasswordHash)
+                registerPasswordMatchErrorLabel.setText("Password does not match!");
+            else {
+                UserServiceDTO userServiceDTO = new UserServiceDTO(email, firstName, lastName, passwordHash);
+                service.createUserAccount(userServiceDTO);
+                registerSuccessMessageLabel.setText("Account created successfully!");
+                firstNameTextField.clear();
+                lastNameTextField.clear();
+                emailTextField.clear();
+                passwordTextField.clear();
+                confirmPasswordTextField.clear();
+            }
+        } catch (ValidationException | AdministrationException ex) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning!");
+            alert.setHeaderText("Register warning!");
+            alert.setContentText(ex.getMessage());
+            alert.showAndWait();
+        }
     }
 
     public void cancel() throws IOException {
